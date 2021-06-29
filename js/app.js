077b1951e7c297b3ad52df96ca21fe7748976d66
DamienCourcoux <damsmailspro@gmail.com>
2021-06-29 16:19:42 +0200

    Dessinons !

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 var app = {
+  row: 4,
+  cell: 6,
   player: {
     x: 0,
     y: 0,
@@ -7,6 +9,24 @@ var app = {
   targetCell: {
     x: 5,
     y: 3
+  },
+  drawBoard: function () {
+    for (var rowIndex = 0; rowIndex < app.row; rowIndex++) {
+      var row = document.createElement("div");
+      row.classList.add("row")
+      for (var cellIndex = 0; cellIndex < app.cell; cellIndex++) {
+        var cell = document.createElement("div");
+        cell.classList.add("cell");
+        row.appendChild(cell);
+      }
+      var board = document.getElementById("board");
+      board.appendChild(row);
+    }
+  },
+  init: function () {
+    app.drawBoard();
   }
 };
+
+app.init()
 
