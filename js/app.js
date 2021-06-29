d505a93a6b7322f36cd1a8acea7a626c610e5fc8
DamienCourcoux <damsmailspro@gmail.com>
2021-06-29 15:34:04 +0200

    Préparer le terrain

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 var app = {
-  init: function () {
-    console.log('init !');
+  player: {
+    x: 0,
+    y: 0,
+    direction: "right"
+  },
+  targetCell: {
+    x: 5,
+    y: 3
   }
 };
 
-document.addEventListener('DOMContentLoaded', app.init);
