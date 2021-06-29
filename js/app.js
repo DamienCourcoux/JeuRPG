94861218655a391a4429774624bdc4e875e5d4d7
DamienCourcoux <damsmailspro@gmail.com>
2021-06-29 19:05:44 +0200

    Rajouter le joueur et la case d'arrivée, mercii Maitre NIB, moi mauvais padawan :)

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@ var app = {
   row: 4,
   cell: 6,
   player: {
-    x: 0,
-    y: 0,
+    x: "0",
+    y: "0",
     direction: "right"
   },
   targetCell: {
-    x: 5,
-    y: 3
+    x: "5",
+    y: "3"
   },
   drawBoard: function () {
     for (var rowIndex = 0; rowIndex < app.row; rowIndex++) {
@@ -17,6 +17,17 @@ var app = {
       for (var cellIndex = 0; cellIndex < app.cell; cellIndex++) {
         var cell = document.createElement("div");
         cell.classList.add("cell");
+        cell.setAttribute("data-x", cellIndex);
+        cell.setAttribute("data-y", rowIndex);
+        var cellX = cell.dataset.x;
+        var cellY = cell.dataset.y;
+        if (cellX === app.targetCell.x && cellY === app.targetCell.y) {
+          cell.classList.add("targetCell");
+        }
+        if (cellX === app.player.x && cellY === app.player.y) {
+          cell.classList.add("player");
+        }
+
         row.appendChild(cell);
       }
       var board = document.getElementById("board");
